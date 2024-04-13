d9129e5b4665b326c3ef6d8b69646b5f24b34394
Ahmad Piracha <ahmadpiracha3@gmail.com>
2024-04-13 21:28:52 +0500

    all routes created

---
+++ routes/contactRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get((req, res) => {
+    res.json({ message: "Get all contacts" })
+});
+
+router.route('/').post((req, res) => {
+    res.json({ message: "Create Contact" })
+});
+
+router.route('/:id').get((req, res) => {
+    res.json({ message: `Get Contact for ${req.params.id}` })
+});
+
+router.route('/:id').put((req, res) => {
+    res.json({ message: `Update Contact for ${req.params.id}` })
+});
+
+router.route('/:id').delete((req, res) => {
+    res.json({ message: `Delete Contact for ${req.params.id}` })
+});
+module.exports = router;
